1daf3a5d5fd1fe26b038ae2b5efc25268123bd02
Petteri <petteri.vattulainen@metropolia.fi>
2020-09-30 11:26:21 +0300

    app.UseHttpsRedirection(); kommentoitu

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace Taustajarjestelmat_Projekti
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //  app.UseHttpsRedirection();
 
             app.UseRouting();
 
